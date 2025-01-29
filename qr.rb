c56fcb591b278e4f6b71667b0fde1ab1b4430902
borvux <bennyjoram@gmail.com>
2025-01-29 01:21:07 -0600

    required the rqrcode gem, added how to use the gem

---
+++ qr.rb
@@ -0,0 +1,7 @@
+require "rqrcode"
+
+# qrcode = RQRCode::QRCode.new("wikipedia qr code")
+# WIFI:T:WPA;S:My wifi network;P:supersecretpassword;;
+# SMSTO:9876543210:Hi Alice! It's
+# png = qrcode.as_png({ :size => 500 })
+# IO.binwrite("sometext.png", png.to_s)
