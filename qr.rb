029eb6d74b633f9a41b87cc0072cc4ebc3d17092
borvux <bennyjoram@gmail.com>
2025-01-29 01:24:09 -0600

    created qr code program

--- qr.rb
+++ qr.rb
@@ -20,15 +20,52 @@ puts
 
 case user_input
 when "1"
-    puts "What is the URL you would like to encode?"
-    puts
+  puts "What is the URL you would like to encode?"
+  puts
+
+  url = gets.chomp
+  puts
+
+  encode = url
 when "2"
-    puts "What is the name of the wifi network?"
-    puts 
+  puts "What is the name of the wifi network?"
+  puts
+
+  wifi_name = gets.chomp
+  puts
+
+  puts "What is the password?"
+  puts
+
+  password = gets.chomp
+  puts
+
+  encode = "WIFI:T:WPA;S:#{wifi_name};P:#{password};;"
 when "3"
-    puts "What is the phone number you want the code to send a text message to?"
-    puts
+  puts "What is the phone number you want the code to send a text message to?"
+  puts
+
+  phone_number = gets.chomp
+  puts
+
+  puts "What do you want to populate the message with?"
+  puts
+
+  content = gets.chomp
+  puts
+
+  encode = "SMSTO:#{phone_number}:#{content}"
 else
-    puts "Goodbye!"
-    exit
+  puts "Goodbye!"
+  exit
 end
+
+puts "What would you like to call the PNG?"
+puts
+
+filename = gets.chomp
+puts
+
+qrcode = RQRCode::QRCode.new("#{encode}")
+png = qrcode.as_png({ :size => 500 })
+IO.binwrite("#{filename}.png", png.to_s)
